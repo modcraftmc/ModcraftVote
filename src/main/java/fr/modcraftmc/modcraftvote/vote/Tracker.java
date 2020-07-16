b28d601ddadb4a233c196336fb1e34be96d8f2a6
manugame <manugameur.pro@gmail.com>
2020-07-16 22:14:32 +0200

    TODO: Fix date check

--- src/main/java/fr/modcraftmc/modcraftvote/vote/Tracker.java
+++ src/main/java/fr/modcraftmc/modcraftvote/vote/Tracker.java
@@ -1,10 +1,15 @@
 package fr.modcraftmc.modcraftvote.vote;
 
+import fr.modcraftmc.modcraftvote.ModcraftVote;
 import fr.modcraftmc.modcraftvote.TimeHelper;
 import fr.modcraftmc.modcraftvote.utils.HttpUtils;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.text.StringTextComponent;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -39,7 +44,26 @@ public class Tracker {
 
                 if (response.status.equalsIgnoreCase("1")) {
 
-                    entity.sendMessage(new StringTextComponent("voted"));
+                    Date date = new Date();
+                    Timestamp currentts = new Timestamp(date.getTime());
+
+                    long current = currentts.getTime();
+                    long datevote = Long.parseLong(response.vote);
+
+
+                    System.out.println(datevote + 5600);
+                    System.out.println(current);
+
+                    if ((datevote + 5600) >= current) {
+
+
+                        if (response.pseudo.equalsIgnoreCase(entity.getName().getString())) {
+                            ModcraftVote.tracker.sendLoot(entity, response);
+                        }
+
+                    } else {
+                        entity.sendMessage(new StringTextComponent("vous avez deja voter"));
+                    }
 
                     timer.cancel();
                     timer.purge();
--- src/main/java/fr/modcraftmc/modcraftvote/vote/VoteTracker.java
+++ src/main/java/fr/modcraftmc/modcraftvote/vote/VoteTracker.java
@@ -1,10 +1,15 @@
 package fr.modcraftmc.modcraftvote.vote;
 
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.text.StringTextComponent;
 
 import java.util.HashMap;
 
-public class VoteTracker implements Runnable {
+public class VoteTracker {
 
     private HashMap<ServerPlayerEntity, Tracker> tracked = new HashMap<>();
 
@@ -19,9 +24,17 @@ public class VoteTracker implements Runnable {
 
     }
 
-    @Override
-    public void run() {
+    public void sendLoot(ServerPlayerEntity entity, VoteResponse response) {
 
+
+        if (entity.inventory.addItemStackToInventory(new ItemStack(Items.DIAMOND))) {
+
+            entity.sendMessage(new StringTextComponent("§eMerci d'avoir voté, vous avez reçu votre récompense."));
+            entity.sendMessage(new StringTextComponent("§eProchain vote disponible dans 1h30."));
+        } else {
+            entity.sendMessage(new StringTextComponent("§eVotre inventaire est plein."));
+        }
 
     }
+
 }
